9444ff98f0a5b82d3d6e427a0ef0e68c340bbc8e
kostia1 <kostia.olejnik1@gmail.com>
2017-12-11 05:10:55 +0200

    init commit

---
+++ js/script.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+  
+
+  $(".slider").owlCarousel({
+  		items: 1,
+  		loop:true,
+  		autoplay:true,
+  		autoplayTimeout:3000
+  	});
+	
+
+  $(".btn-nav").on("click",function() {
+		var target = $(this).data("target");
+		$(target).toggleClass("nav__list--open");
+	});
+
+  $("a.masters__yk").click(function () {
+var elementClick = $(this).attr("href")
+var destination = $(elementClick).offset().top;
+jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 800);
+return false;
+});
+});
+
+
+
+$(function() {
+ 
+$(window).scroll(function() {
+ 
+if($(this).scrollTop() != 0) {
+  
+ 
+$('#up').fadeIn();
+ 
+} else {
+ 
+$('#up').fadeOut();
+
+ 
+}
+ 
+});
+ 
+$('#up').click(function() {
+ 
+$('body,html').animate({scrollTop:0},800);
+
+ 
+});
+ 
+});
+ 
+var canvas = document.getElementById('c3');
+var ctx = canvas.getContext('2d');
+
+    ctx.beginPath();
+    ctx.moveTo(20,50);
+    ctx.lineTo(40,50);
+    ctx.lineTo(40,30);
+    ctx.lineTo(40,30);
+    ctx.lineTo(50,30);
+    ctx.lineTo(30,10);
+    ctx.lineTo(10,30);
+    ctx.lineTo(20,30);    
+    ctx.lineTo(20,50);
+    ctx.fill();
+
+
+
+
